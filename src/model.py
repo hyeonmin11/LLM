f7d1953e867bef793bc7a50eda851b16e4fb2d1e
hyeonmin11 <79415795+hyeonmin11@users.noreply.github.com>
2023-05-24 03:39:07 +0900

    finish 2-a, doing 1-a

--- src/model.py
+++ src/model.py
@@ -8,4 +8,12 @@ from transformers import *
 from torch.utils.data import Dataloader
 
 
-tokenizer = GPT2Tokenizer.from_pretrained('heegyu/gpt2-emotion')
+from transformers import AutoModel, AutoTokenizer
+
+
+GPTtokenizer = GPT2Tokenizer.from_pretrained('heegyu/gpt2-emotion')
+
+
+model_name = "textattack/bert-base-uncased-SST-2"
+BERTmodel = AutoModel.from_pretrained(model_name)
+BERTtokenizer = AutoTokenizer.from_pretrained(model_name)
